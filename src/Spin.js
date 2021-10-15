1da2dd72b5b05e684866cdf51de9e0b03ba76b06
PetrRNNT <58854342+PetrRNNT@users.noreply.github.com>
2021-10-15 17:27:43 +0200

    Create a React app and publish it to GitHub Pages

---
+++ src/Spin.js
@@ -0,0 +1,21 @@
+import { useSelector } from 'react-redux';
+import Loader from 'react-loader-spinner';
+
+const Spin = (props) => {
+    const spinner = useSelector(state => state.appReducer.loading)
+    return (
+        // spinner && (
+            <div className="loader-styles">
+                <Loader
+                    type="TailSpin"
+                    color="#00BFFF"
+                    height={100}
+                    width={100}
+                    visible={spinner}
+                />
+            </div>
+        // )
+    )
+}
+
+export default Spin;
